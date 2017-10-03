3317e99e42a8001c3886d560e29d429b2e271039
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-03 22:25:16 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,50 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def move(board, index, value='X')
+
+update_array_at_with(board,index,value)
+display_board(board)
+
+end
+
+def is_number?(usr_input)
+    usr_input.to_f.to_s == usr_input.to_s || usr_input.to_i.to_s == usr_input.to_s
+
+
+  end
+
 # code your input_to_index and move method here!
+def input_to_index(usr_input)
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+if is_number?(usr_input)
+
+
+  index = usr_input.to_i - 1
+
+if index < 0 || index > 8
+
+   puts "Your number must be between 1 and 9."
+
+else
+
+  move(board, index, value='X')
+end
+else
+
+  index = -1
+
+
+
+puts "#{usr_input} is not a number"
+
+#display_board(board)
+
+end
+
+end
+
+def update_array_at_with(array, index, value)
+  array[index] = value
+end

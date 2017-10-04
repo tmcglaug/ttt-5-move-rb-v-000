fe153698d4c5e242477e682045ad4ce893f493b1
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-04 01:14:21 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,9 +8,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move(board, index, value='X')
+def move(board, usr_input.to_i - 1, value='X')
 
-board[index.to_i] = value;
+board[usr_input.to_i - 1] = value;
 
 
 end
